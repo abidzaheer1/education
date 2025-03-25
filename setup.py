0df57e2631bf4097454cfab2e82b3330ffe6ad17
abidzaheer1 <abidzaheer444@gmail.com>
2025-03-26 04:57:46 +0530

    renamed to education

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ with open("requirements.txt") as f:
 	install_requires = f.read().strip().split("\n")
 
 # get version from __version__ variable in education/__init__.py
-from asxoraeducation import __version__ as version
+from education import __version__ as version
 
 setup(
-	name="asxoraeducation",
+	name="education",
 	version=version,
 	description="asxoraeducation",
 	author="Asxora Technology LLP",
